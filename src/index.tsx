51a5fd30f4990fb2edc04bff45b9c8228cbc35a2
nichoguimbaolibot <nichoguimbaolibot@gmail.com>
2019-08-22 13:31:09 +0800

    remove unrelated codes

--- src/index.tsx
+++ src/index.tsx
@@ -2,36 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'rxjs';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import { createEpicMiddleware } from 'redux-observable';
 import App from './appRoot/App';
-import rootEpic from './store/rootEpics';
-import rootReducer from './store/rootReducers';
 import * as serviceWorker from './serviceWorker';
 
 import 'antd/dist/antd.css';
 import './assets/css/main.scss';
 
-const composeEnhancers =
-  /* tslint:disable-next-line:no-string-literal */
-  (window['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as typeof compose) || compose;
-
-const epicMiddlerware = createEpicMiddleware();
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(epicMiddlerware)),
-);
-
-epicMiddlerware.run(rootEpic);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
